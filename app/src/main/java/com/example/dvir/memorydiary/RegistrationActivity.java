8d49d9f3dbdd3bf6910f9d081c3c08b336125629
Dvir Barzilay <dvir.barzilay@gmail.com>
2019-03-05 22:38:22 +0200

    Reset Password layout design

--- app/src/main/java/com/example/dvir/memorydiary/RegistrationActivity.java
+++ app/src/main/java/com/example/dvir/memorydiary/RegistrationActivity.java
@@ -3,7 +3,7 @@ package com.example.dvir.memorydiary;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Registration extends AppCompatActivity {
+public class RegistrationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
