68bff2f68adbd1e5923dc8c59ab0d853c1f07aaa
Kineruth <naharykin@gmail.com>
2019-03-06 11:23:38 +0200

    Fixed some bug in Activity folder & changed values folder

--- app/src/androidTest/java/memory/memoryDiary/Activity/Start/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/memory/memoryDiary/Activity/Start/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.dvir.memorydiary;
+package memory.memoryDiary.Activity.Start;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.dvir.memorydiary", appContext.getPackageName());
+        assertEquals("com.memoryDiary.Activity.Start", appContext.getPackageName());
     }
 }
