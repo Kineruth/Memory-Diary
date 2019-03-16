befad51057b5a969445e6674f8b23fdbda29f01a
Dvir Barzilay <dvir.barzilay@gmail.com>
2019-03-16 20:56:00 +0200

    Taking a picture & display the picture

--- app/src/main/java/com/memoryDiary/Fragment/CameraFragment.java
+++ app/src/main/java/com/memoryDiary/Fragment/CameraFragment.java
@@ -2,6 +2,7 @@ package com.memoryDiary.Fragment;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
 import android.net.Uri;
@@ -14,15 +15,20 @@ import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.Toast;
 
+import com.memoryDiary.Activity.Start.ShowCaptureActivity;
 import com.memoryDiary.R;
 
 import java.io.IOException;
+import java.util.List;
 
 
 public class CameraFragment extends Fragment implements SurfaceHolder.Callback {
     Camera camera;
+
+    Camera.PictureCallback jpegCallback;
 
     SurfaceView cameraSurfaceView;
     SurfaceHolder cameraSurfaceHolder;
@@ -39,13 +45,34 @@ public class CameraFragment extends Fragment implements SurfaceHolder.Callback {
         View view = inflater.inflate(R.layout.fragment_camera, container, false);
         cameraSurfaceView = view.findViewById(R.id.cameraSurfaceView);
         cameraSurfaceHolder = cameraSurfaceView.getHolder();
-        if(ActivityCompat.checkSelfPermission(getContext(), android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
-            ActivityCompat.requestPermissions(getActivity(), new String[] {android.Manifest.permission.CAMERA}, CAMERA_REQUEST_CODE);
+        if(ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(getActivity(), new String[] {Manifest.permission.CAMERA}, CAMERA_REQUEST_CODE);
         }else {
             cameraSurfaceHolder.addCallback(this);
             cameraSurfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
         }
+
+        Button captureButton = view.findViewById(R.id.captureButton);
+        captureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                captureImage();
+            }
+        });
+        jpegCallback = new Camera.PictureCallback(){
+            @Override
+            public void onPictureTaken(byte[] data, Camera camera) {
+                Intent intent = new Intent(getActivity(), ShowCaptureActivity.class);
+                intent.putExtra("capture", data);
+                startActivity(intent);
+                return;
+            }
+        };
         return view;
+    }
+
+    private void captureImage() {
+        camera.takePicture(null, null, jpegCallback);
     }
 
     @Override
@@ -58,6 +85,17 @@ public class CameraFragment extends Fragment implements SurfaceHolder.Callback {
         camera.setDisplayOrientation(90);
         parameters.setPreviewFrameRate(30);
         parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
+
+        Camera.Size bestSize = null;
+        List<Camera.Size> sizeList = camera.getParameters().getSupportedPreviewSizes();
+        bestSize = sizeList.get(0);
+        for(int i = 1; i < sizeList.size(); i++){
+            if((sizeList.get(i).width * sizeList.get(i).height) > (bestSize.width * bestSize.height)){
+                bestSize = sizeList.get(i);
+            }
+        }
+        parameters.setPictureSize(bestSize.width, bestSize.height);
+
         camera.setParameters(parameters);
 
         try {
