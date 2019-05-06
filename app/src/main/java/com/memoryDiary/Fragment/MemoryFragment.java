98adf0d4327c639639d2c978f1c6f88e7c7f44c0
Kineruth <naharykin@gmail.com>
2019-05-06 20:23:04 +0300

    changed memory activity to fragment and added a slide option- need to check if it works

--- app/src/main/java/com/memoryDiary/Fragment/MemoryFragment.java
+++ app/src/main/java/com/memoryDiary/Fragment/MemoryFragment.java
@@ -1,17 +1,22 @@
-package com.memoryDiary.Activity.Memory;
+package com.memoryDiary.Fragment;
 
 import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
@@ -19,6 +24,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.memoryDiary.Activity.Memory.AddMemoryActivity;
 import com.memoryDiary.Adapter.MemoryAdapter;
 import com.memoryDiary.Entity.Memory;
 import com.memoryDiary.Holder.UserDataHolder;
@@ -27,8 +33,9 @@ import com.memoryDiary.R;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MemoryViewActivity extends AppCompatActivity {
+public class MemoryFragment extends Fragment {
 
+    private View mView;
     private Toolbar mToolbar;
     private FloatingActionButton mFabAdd;
     private RecyclerView memoryRecyclerView;
@@ -36,33 +43,34 @@ public class MemoryViewActivity extends AppCompatActivity {
     private List<Memory> memories;
     private DatabaseReference mData;
 
+//    public static MemoryFragment newInstance() {
+//        MemoryFragment mFragment = new MemoryFragment();
+//        return mFragment;
+//    }
+
+    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_memory_view);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        mView = inflater.inflate(R.layout.fragment_memory, container, false);
         initFields();
         initFireBase();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        initRecyclerView();
+        return mView;
     }
 
     /**
      * Initialization the connection of the fields in xml file to their activities.
      */
     private void initFields() {
-        mToolbar = findViewById(R.id.memory_toolbar);
-        setSupportActionBar(mToolbar);
+        mToolbar = mView.findViewById(R.id.memory_toolbar);
+        ((AppCompatActivity)getActivity()).setSupportActionBar(mToolbar);
+        setHasOptionsMenu(true);
 //        setTitle(R.id.memory_toolbar);
-        mFabAdd = findViewById(R.id.memory_add_floating_button);
+        mFabAdd = mView.findViewById(R.id.memory_add_floating_button);
         memories = new ArrayList<>();
-        memoryRecyclerView = findViewById(R.id.memory_recyclerview);
+        memoryRecyclerView = mView.findViewById(R.id.memory_recyclerview);
         memoryRecyclerView.setHasFixedSize(true);
-        memoryRecyclerView.setLayoutManager(new GridLayoutManager(this,3));
-        adapter = new MemoryAdapter(this, memories);
+        memoryRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(),4));
+        adapter = new MemoryAdapter(getActivity(), memories);
         memoryRecyclerView.setAdapter(adapter);
 
         mFabAdd.setOnClickListener(new View.OnClickListener() {
@@ -101,10 +109,9 @@ public class MemoryViewActivity extends AppCompatActivity {
      * @return true to be displayed.
      */
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        super.onCreateOptionsMenu(menu);
-        getMenuInflater().inflate(R.menu.activity_memory_toolbar_menu,menu);
-        return true;
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.activity_memory_toolbar_menu, menu);
+        super.onCreateOptionsMenu(menu,inflater);
     }
 
     /**
@@ -116,16 +123,16 @@ public class MemoryViewActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         super.onOptionsItemSelected(item);
         if(item.getItemId() == R.id.memory_toolbar_search){
-            Toast.makeText(this, "Search Clicked", Toast.LENGTH_SHORT).show();
+            Toast.makeText(getActivity(), "Search", Toast.LENGTH_SHORT).show();
         }
         if(item.getItemId() == R.id.memory_toolbar_settings){
-            Toast.makeText(this, "Settings Clicked", Toast.LENGTH_SHORT).show();
+            Toast.makeText(getActivity(), "Settings", Toast.LENGTH_SHORT).show();
         }
         return true;
     }
 
     private void addMemoryActivity() {
-        Intent intent = new Intent(this, AddMemoryActivity.class);
+        Intent intent = new Intent(getActivity(), AddMemoryActivity.class);
         startActivity(intent);
     }
 
